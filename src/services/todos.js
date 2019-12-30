1534adc776f7c4eefa9f96eab6ac3f575c571929
W2AlharbiMe <w2.alharbi.me@Gmail.com>
2019-12-30 11:31:50 +0300

    todos service

---
+++ src/services/todos.js
@@ -0,0 +1,32 @@
+const axios = require('axios');
+
+const _API = "https://jsonplaceholder.typicode.com/todos";
+
+
+const get = () => {
+    return axios.get(_API);
+}
+
+const show = id => {
+    return axios.get(`${_API}/${id}`);
+}
+
+const create = todo => {
+    return axios.post(_API, todo);
+}
+
+const update = (id, todo) => {
+    return axios.post(`${_API}/${id}`, todo);
+}
+
+const destroy = id => {
+    return axios.delete(`${_API}/${id}`);
+}
+
+module.exports = {
+    get,
+    show,
+    create,
+    update,
+    destroy
+};
